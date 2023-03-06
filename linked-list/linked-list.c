eee25d8e107c712236f06b6af6c1d44810f7af24
Veldas R Durai <veldasrdurai72@gmail.com>
2023-03-06 17:25:30 +0530

    updated linked-list

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -6,18 +6,19 @@ typedef struct node {
 } NODE;
 
 NODE* createNode();
-NODE* nodeAtPostion(NODE* HEAD, int position);
+// INSERSION
 void insertAtBegining(int data, NODE* HEAD);
 void insertAtMiddle(int data, NODE* HEAD, int position);
 void insertAtEnd(int data, NODE* HEAD);
-void traversal(NODE* HEAD){
-    NODE* temp = HEAD;
-    printf("\n\nList elements are - \n");
-    while(temp != NULL) {
-        printf("%d --->",temp->data);
-        temp = temp->next;
-    }
-}
+// DELETION
+void deleteAtBegining(NODE* HEAD);
+void deleteAtMiddle(int data, NODE* HEAD, int position);
+void deleteAtEnd(int data, NODE* HEAD);
+void traversal(NODE* HEAD)
+// SEARCH
+NODE* nodeAtPostion(NODE* HEAD, int position);
+int search(NODE* HEAD, int data);
+// SORT
 
 int main() {
     NODE* HEAD;
@@ -34,11 +35,10 @@ NODE* createNode(){
 NODE* nodeAtPostion(NODE* HEAD, int position){
     NODE* temp = HEAD;
     int currentPosition = 0;
-    while( temp->next !=NULL ){
-        if( currentPosition == position ){
-            return temp
-        }
+    while( temp != NULL ){
+        if( currentPosition == position ) return temp;
         temp = temp->next;
+        currentPosition++;
     }
     return NULL;
 }
@@ -72,4 +72,51 @@ void insertAtEnd(int data, NODE* HEAD){
     }
     temp->next = newNode;
     newNode.data = data;
+}
+void deleteAtBegining(NODE* HEAD){
+    NODE* temp = HEAD;
+    HEAD = HEAD->next;
+    free(temp);
+}
+void deleteAtMiddle(NODE* HEAD, int position){
+    NODE* prevNode = nodeAtPostion(HEAD,position-1);
+    if(prevNode==NULL) return;
+
+    NODE* temp = prevNode->next;
+    prevNode->next = prevNode->next->next;
+    free(temp);
+}
+void deleteAtEnd(int data, NODE* HEAD){
+    if( HEAD->next == NULL ){
+        deleteAtBegining(HEAD);
+        return;
+    }
+    NODE* prevTemp = HEAD; 
+    NODE* temp = HEAD->next;
+    while( temp->next != NULL ){
+        prevTemp = temp;
+        temp = temp->next;
+    }
+    prevTemp->next = NULL;
+    free(temp);
+}
+
+void traversal(NODE* HEAD){
+    NODE* temp = HEAD;
+    printf("\n\nList elements are - \n");
+    while(temp != NULL) {
+        printf("%d --->",temp->data);
+        temp = temp->next;
+    }
+}
+
+
+int search(NODE* HEAD, int data){
+    NODE* temp = HEAD;
+    int currentPosition = 0;
+    while( temp->next!=NULL ){
+        if(temp->data == data) return currentPosition;
+        temp = temp->next;
+        currentPosition++;
+    }
 }
